53ffb2df80d57072174e2ba7433cd799aabc7534
袁孟華 <mason1719.cs10@nycu.edu.tw>
2024-01-03 03:29:21 +0800

    選取指定日期購票

--- src/tix_bot.go
+++ src/tix_bot.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("開始執行腳本")
+
+	allocatorCtx, cancel := chromedp.NewExecAllocator(context.Background(), append(
+		chromedp.DefaultExecAllocatorOptions[:],
+		// 確保不包括 chromedp.Headless 選項
+		chromedp.Flag("headless", false), // 這行可以明確禁用無頭模式（通常不是必需的，除非在默認選項中包含了無頭模式）
+	)...)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocatorCtx)
+	defer cancel()
+
+	sidCookie := &network.CookieParam{
+		Name:   "SID",
+		Value:  "qav8f6f75gbq5ivk9eapognjjb",
+		Domain: "tixcraft.com",
+	}
+
+	log.Println("正在訪問網站")
+	var ticketPageURL string
+	err := chromedp.Run(ctx,
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			return network.SetCookies([]*network.CookieParam{sidCookie}).Do(ctx)
+		}),
+		chromedp.Navigate("https://tixcraft.com/activity/detail/24_lesmis"),
+		chromedp.WaitVisible(`.buy`, chromedp.ByQuery),
+		chromedp.AttributeValue(`.buy a`, "href", &ticketPageURL, nil),
+		chromedp.Click(`.buy`, chromedp.NodeVisible),
+		chromedp.WaitVisible(`#gameList`, chromedp.ByQuery),
+		chromedp.Click(`//tr[contains(., '2024/01/07 (日)  15:00')]//button[contains(@class, 'btn-primary')]`, chromedp.BySearch),
+	)
+
+	if err != nil {
+		log.Printf("遇到錯誤：%v", err)
+	} else {
+		log.Println("腳本執行完成")
+	}
+
+	var input string
+	fmt.Println("按下 Enter 鍵結束...")
+	fmt.Scanln(&input)
 }
