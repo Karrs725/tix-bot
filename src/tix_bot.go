4f5239d534d60548c6b520e002c10c4ce1f3ac95
Karrs725 <karrs600914@gmail.com>
2024-01-04 03:41:24 +0800

    自訂輸入

--- src/tix_bot.go
+++ src/tix_bot.go
@@ -7,10 +7,77 @@ import (
 
 	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
+
+type InputData struct {
+	url  string
+	sid  string
+	date string
+	num  string
+}
+
+var app = tview.NewApplication()
+var text = tview.NewTextView().
+	SetTextColor(tcell.ColorGreen).
+	SetText("(a) to add a new contact \n(q) to quit")
+var form = tview.NewForm()
+var pages = tview.NewPages()
+var flex = tview.NewFlex()
+var inputdata InputData
+
+func setInputData() *tview.Form {
+
+	form.AddInputField("Url", "", 20, nil, func(url string) {
+		inputdata.url = url
+	})
+
+	form.AddInputField("SID", "", 20, nil, func(sid string) {
+		inputdata.sid = sid
+	})
+
+	form.AddInputField("Date", "", 20, nil, func(date string) {
+		inputdata.date = date
+	})
+
+	form.AddInputField("Num", "", 20, nil, func(num string) {
+		inputdata.num = num
+	})
+
+	form.AddButton("Save", func() {
+		pages.SwitchToPage("Menu")
+	})
+
+	return form
+}
 
 func main() {
 	log.Println("開始執行腳本")
+
+	// get input data
+
+	flex.SetDirection(tview.FlexRow).
+		AddItem(text, 0, 1, false)
+
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 113 {
+			app.Stop()
+		} else if event.Rune() == 97 {
+			form.Clear(true)
+			setInputData()
+			pages.SwitchToPage("Add Input Data")
+		}
+		return event
+	})
+
+	pages.AddPage("Menu", flex, true, true)
+	pages.AddPage("Add Input Data", form, true, false)
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+	//
 
 	allocatorCtx, cancel := chromedp.NewExecAllocator(context.Background(), append(
 		chromedp.DefaultExecAllocatorOptions[:],
@@ -24,7 +91,7 @@ func main() {
 
 	sidCookie := &network.CookieParam{
 		Name:   "SID",
-		Value:  "qav8f6f75gbq5ivk9eapognjjb",
+		Value:  inputdata.sid,
 		Domain: "tixcraft.com",
 	}
 
@@ -34,7 +101,7 @@ func main() {
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			return network.SetCookies([]*network.CookieParam{sidCookie}).Do(ctx)
 		}),
-		chromedp.Navigate("https://tixcraft.com/activity/detail/24_lesmis"),
+		chromedp.Navigate(inputdata.url),
 
 		chromedp.WaitVisible(`.buy`, chromedp.ByQuery),
 		//chromedp.Click(`#onetrust-accept-btn-handler`, chromedp.NodeVisible),
@@ -48,7 +115,7 @@ func main() {
 		chromedp.Click(`li.select_form_b:first-of-type`, chromedp.ByQuery),
 
 		chromedp.WaitVisible(`#TicketForm_ticketPrice_03`, chromedp.ByQuery),
-		chromedp.SetValue(`#TicketForm_ticketPrice_03`, "2", chromedp.ByQuery),
+		chromedp.SetValue(`#TicketForm_ticketPrice_03`, inputdata.num, chromedp.ByQuery),
 	)
 
 	if err != nil {
